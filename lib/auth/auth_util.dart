de3bae3a8063c74d75fa03ca40fe3e4349f2a690
JasonFeng365 <34261800+JasonFeng365@users.noreply.github.com>
2025-02-10 11:35:29 -0800

    Make Create Account page

--- lib/auth/auth_util.dart
+++ lib/auth/auth_util.dart
@@ -48,6 +48,44 @@ class AuthUtil {
     return true;
   }
 
+  static Future<bool> createAccount(String username, String password) async {
+    print("In AuthUtil.createAccount");
+    print(username);
+    print(password);
+    if (isLoggedIn()) {
+      throw "Already logged in!";
+    }
+
+    final requestBody = {
+      "username": username,
+      "password": password
+    };
+
+    print(requestBody);
+    print(jsonEncode(requestBody));
+
+    final uri = Uri(
+      scheme: "https",
+      host: "cycleguardbackend-638241752910.us-central1.run.app",
+      path: "/account/create",
+    );
+
+    print(uri);
+    final response = await http.post(uri, body: jsonEncode(requestBody), headers: {'Content-Type':'application/json'});
+
+    final newToken = response.body;
+    print("Got response from server: $newToken");
+
+    if (newToken.length != 16) {
+      print("Login failed!");
+      return false;
+    }
+
+    _token = newToken;
+
+    return true;
+  }
+
   static void logout() {
 
   }

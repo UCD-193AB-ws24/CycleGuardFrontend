345bb1534eb96ecb7b044e0d282a950106079f59
Andrew <andrewh965@gmail.com>
2025-02-20 10:50:19 -0800

    -added map ask andrew for api key if you for some reason need to use map

--- lib/pages/routes_page.dart
+++ lib/pages/routes_page.dart
@@ -1,8 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'package:location/location.dart';
 
-class RoutesPage extends StatelessWidget {
+//import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
+class RoutesPage extends StatefulWidget {
   @override
   Widget build(BuildContext context) {
     return Center(child: Text('Routes Page'));
   }
+
+  @override
+  State<StatefulWidget> createState() => mapState();
+}
+
+class mapState extends State<RoutesPage>{
+  late GoogleMapController mapController;
+  final locationController = Location();
+  late LatLng center = new LatLng(0,0);
+
+  @override
+  void initState(){
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async => await fetchLocationUpdates());
+  }
+
+  void onMapCreated(GoogleMapController controller){
+    mapController=controller;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: GoogleMap(
+          initialCameraPosition: CameraPosition(
+              target: center,
+              zoom: 11.0)
+      ),
+    );
+  }
+  Future<void> fetchLocationUpdates() async{
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+
+    serviceEnabled = await locationController.serviceEnabled();
+    if(serviceEnabled){
+      serviceEnabled = await locationController.requestService();
+    }else{
+      return;
+    }
+
+    permissionGranted = await locationController.hasPermission();
+    if(permissionGranted == PermissionStatus.denied){
+      permissionGranted = await locationController.requestPermission();
+      if(permissionGranted!= PermissionStatus.granted){
+        return;
+      }
+    }
+    locationController.onLocationChanged.listen((center){});
+    if(center.latitude != null && center.longitude != null){
+      setState(() {
+        center = LatLng(
+            center.latitude,
+            center.longitude);
+      });
+    }
+  }
+
 }

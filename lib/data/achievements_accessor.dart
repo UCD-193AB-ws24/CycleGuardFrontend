0c86115b496abc585f4595062d6d98b17ccd0ccf
Jason Feng <34261800+JasonFeng365@users.noreply.github.com>
2025-02-24 19:09:56 -0800

    Achievements and Dummy Stats
    * Add... everything?
    
    * Reset host to Cloud Run, from ngrok. Whoops!
    
    * Fix typos!

---
+++ lib/data/achievements_accessor.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+import 'package:cycle_guard_app/auth/requests_util.dart';
+class AchievementInfoAccessor {
+  AchievementInfoAccessor._();
+
+  static Future<AchievementInfo> getAchievementInfo() async {
+    final response = await RequestsUtil.getWithToken("/achievements/getAchievements");
+
+    if (response.statusCode == 200) {
+      print(response.body);
+      throw Exception("TODO: implement JSON parsing. Look at health_info_accessor and user_stats_accessor for an example");
+      return AchievementInfo.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+    } else {
+      throw Exception('Failed to get achievement info');
+    }
+  }
+}
+
+// TODO: change to Achievement Info variables. May need a separate "AchievementProgress" class, per achievement ID
+class AchievementInfo {
+  final int heightInches;
+  final int weightPounds;
+  final int ageYears;
+
+  const AchievementInfo({required this.heightInches, required this.weightPounds, required this.ageYears});
+
+  factory AchievementInfo.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        "heightInches": int heightInches,
+        "weightPounds": int weightPounds,
+        "ageYears": int ageYears
+      } => AchievementInfo(
+        heightInches: heightInches,
+        weightPounds: weightPounds,
+        ageYears: ageYears
+      ),
+      _ => throw const FormatException("failed to load HealthInfo"),
+    };
+  }
+}
---
+++ lib/data/week_history_accessor.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+import 'package:cycle_guard_app/auth/requests_util.dart';
+class WeekHistoryAccessor {
+  WeekHistoryAccessor._();
+
+  static Future<WeekHistory> getWeekHistory() async {
+    final response = await RequestsUtil.getWithToken("/history/getWeekHistory");
+
+    if (response.statusCode == 200) {
+      print(response.body);
+      throw Exception("TODO: implement JSON parsing. Look at health_info_accessor and user_stats_accessor for an example");
+      // return WeekHistory.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+    } else {
+      throw Exception('Failed to get health info');
+    }
+  }
+}
+
+// TODO: change to Week History variables. May need to create a separate "DayHistory" class.
+class WeekHistory {
+  final int heightInches;
+  final int weightPounds;
+  final int ageYears;
+
+  const WeekHistory({required this.heightInches, required this.weightPounds, required this.ageYears});
+
+  factory WeekHistory.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        "heightInches": int heightInches,
+        "weightPounds": int weightPounds,
+        "ageYears": int ageYears
+      } => WeekHistory(
+        heightInches: heightInches,
+        weightPounds: weightPounds,
+        ageYears: ageYears
+      ),
+      _ => throw const FormatException("failed to load HealthInfo"),
+    };
+  }
+}

06901eb565fc1dd970e003391d4df4f7d7701c33
Jason Feng <34261800+JasonFeng365@users.noreply.github.com>
2025-02-26 22:11:39 -0800

    Finish achievements and week history parsing (#14)

--- lib/data/achievements_accessor.dart
+++ lib/data/achievements_accessor.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:cycle_guard_app/auth/requests_util.dart';
+import 'package:flutter/semantics.dart';
 class AchievementInfoAccessor {
   AchievementInfoAccessor._();
 
@@ -9,7 +10,6 @@ class AchievementInfoAccessor {
 
     if (response.statusCode == 200) {
       print(response.body);
-      throw Exception("TODO: implement JSON parsing. Look at health_info_accessor and user_stats_accessor for an example");
       return AchievementInfo.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
     } else {
       throw Exception('Failed to get achievement info');
@@ -17,26 +17,75 @@ class AchievementInfoAccessor {
   }
 }
 
-// TODO: change to Achievement Info variables. May need a separate "AchievementProgress" class, per achievement ID
-class AchievementInfo {
-  final int heightInches;
-  final int weightPounds;
-  final int ageYears;
+class AchievementProgress {
+  final int currentProgress;
+  final bool complete;
 
-  const AchievementInfo({required this.heightInches, required this.weightPounds, required this.ageYears});
+  const AchievementProgress({required this.currentProgress, required this.complete});
+
+  factory AchievementProgress.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+      "currentProgress": int currentProgress,
+      "complete": bool complete,
+      } => AchievementProgress(
+          currentProgress: currentProgress,
+          complete: complete,
+      ),
+      _ => throw const FormatException("failed to load AchievementProgress"),
+    };
+  }
+
+  @override
+  String toString() {
+    return 'AchievementProgress{currentProgress: $currentProgress, complete: $complete}';
+  }
+}
+
+class AchievementInfo {
+  // final String username;
+  final Map<int, AchievementProgress> achievementProgressMap;
+
+  const AchievementInfo({required this.achievementProgressMap});
+
+  static Map<int, AchievementProgress> _parseAchievementProgressMap(Map<String, dynamic> stringMap) {
+    Map<int, AchievementProgress> intMap = {};
+
+    for (var entry in stringMap.entries) {
+      intMap[int.parse(entry.key)] = AchievementProgress.fromJson(entry.value);
+    }
+
+    return intMap;
+  }
 
   factory AchievementInfo.fromJson(Map<String, dynamic> json) {
     return switch (json) {
       {
-        "heightInches": int heightInches,
-        "weightPounds": int weightPounds,
-        "ageYears": int ageYears
+        // "username": String username,
+        "achievementProgressMap": Map<String, dynamic> achievementProgressMap,
       } => AchievementInfo(
-        heightInches: heightInches,
-        weightPounds: weightPounds,
-        ageYears: ageYears
+          // username: username,
+          achievementProgressMap: _parseAchievementProgressMap(achievementProgressMap),
       ),
-      _ => throw const FormatException("failed to load HealthInfo"),
+      _ => throw const FormatException("failed to load AchievementInfo"),
     };
+  }
+
+  @override
+  String toString() {
+    return 'AchievementInfo{achievementProgressMap: $achievementProgressMap}';
+  }
+
+  List<bool> getCompletedAchievements() {
+    List<bool> res = List<bool>.filled(achievementProgressMap.length, false, growable: false);
+
+    for (var entry in achievementProgressMap.entries) {
+      int idx = entry.key;
+      bool complete = entry.value.complete;
+
+      res[idx] = complete;
+    }
+
+    return res;
   }
 }
--- lib/data/week_history_accessor.dart
+++ lib/data/week_history_accessor.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:cycle_guard_app/auth/requests_util.dart';
+import 'package:get_storage/get_storage.dart';
 class WeekHistoryAccessor {
   WeekHistoryAccessor._();
 
@@ -8,35 +9,69 @@ class WeekHistoryAccessor {
     final response = await RequestsUtil.getWithToken("/history/getWeekHistory");
 
     if (response.statusCode == 200) {
-      print(response.body);
-      throw Exception("TODO: implement JSON parsing. Look at health_info_accessor and user_stats_accessor for an example");
-      // return WeekHistory.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+      return WeekHistory.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
     } else {
       throw Exception('Failed to get health info');
     }
   }
 }
 
-// TODO: change to Week History variables. May need to create a separate "DayHistory" class.
-class WeekHistory {
-  final int heightInches;
-  final int weightPounds;
-  final int ageYears;
+class DayHistory {
+  final double distance, calories, time;
 
-  const WeekHistory({required this.heightInches, required this.weightPounds, required this.ageYears});
+  const DayHistory({required this.distance, required this.calories, required this.time});
+
+  factory DayHistory.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+      "distance": String distance,
+      "calories": String calories,
+      "time": String time,
+      } => DayHistory(
+        distance: double.parse(distance),
+        calories: double.parse(calories),
+        time: double.parse(time),
+      ),
+      _ => throw const FormatException("failed to load DayHistory"),
+    };
+  }
+
+  @override
+  String toString() {
+    return 'DayHistory{distance: $distance, calories: $calories, time: $time}';
+  }
+}
+
+class WeekHistory {
+  // final String username;
+  final Map<int, DayHistory> dayHistoryMap;
+
+  const WeekHistory({required this.dayHistoryMap});
+
+  static Map<int, DayHistory> _parseDayHistoryMap(Map<String, dynamic> stringMap) {
+    Map<int, DayHistory> intMap = {};
+
+    for (var entry in stringMap.entries) {
+      intMap[int.parse(entry.key)] = DayHistory.fromJson(entry.value);
+    }
+
+    return intMap;
+  }
 
   factory WeekHistory.fromJson(Map<String, dynamic> json) {
     return switch (json) {
       {
-        "heightInches": int heightInches,
-        "weightPounds": int weightPounds,
-        "ageYears": int ageYears
+      "dayHistoryMap": Map<String, dynamic> dayHistoryMap,
       } => WeekHistory(
-        heightInches: heightInches,
-        weightPounds: weightPounds,
-        ageYears: ageYears
+        // username: username,
+        dayHistoryMap: _parseDayHistoryMap(dayHistoryMap),
       ),
-      _ => throw const FormatException("failed to load HealthInfo"),
+      _ => throw const FormatException("failed to load WeekHistory"),
     };
+  }
+
+  @override
+  String toString() {
+    return 'WeekHistory{dayHistoryMap: $dayHistoryMap}';
   }
 }

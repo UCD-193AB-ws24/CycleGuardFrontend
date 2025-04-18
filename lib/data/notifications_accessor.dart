0dc71810ea9584d8a3c372e3f3c46e63a81f0f5d
Jason Feng <34261800+JasonFeng365@users.noreply.github.com>
2025-04-17 19:28:20 -0700

    Add notifications accessors (#52)
    * Add profileIcon: socialPage needs refactoring
    
    * Fix POSTs not updating new fields
    
    * sta()

---
+++ lib/data/notifications_accessor.dart
@@ -0,0 +1,111 @@
+import 'dart:convert';
+
+import 'package:cycle_guard_app/auth/requests_util.dart';
+import 'package:cycle_guard_app/data/single_trip_history.dart';
+import 'package:get_storage/get_storage.dart';
+class NotificationsAccessor {
+  NotificationsAccessor._();
+
+  static Future<NotificationList> getNotifications() async {
+    final response = await RequestsUtil.getWithToken("/notifications/getNotifications");
+
+    if (response.statusCode == 200) {
+      return NotificationList.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+    } else {
+      throw Exception('Failed to get notifications: error code ${response.statusCode}');
+    }
+  }
+
+  static Future<NotificationList> addNotification(Notification notification) async {
+    final body = notification.toJson();
+    final response = await RequestsUtil.postWithToken("/notifications/addNotification", body);
+
+    if (response.statusCode == 200) {
+      return NotificationList.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+    } else {
+      throw Exception('Failed to add notification: error message ${response.body}');
+    }
+  }
+
+  static Future<NotificationList> deleteNotification(Notification notification) async {
+    final body = notification.toJson();
+    final response = await RequestsUtil.postWithToken("/notifications/deleteNotification", body);
+
+    if (response.statusCode == 200) {
+      return NotificationList.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+    } else {
+      throw Exception('Failed to delete notification: error message ${response.body}');
+    }
+  }
+}
+
+class NotificationList {
+  final String username;
+  final List<Notification> notifications;
+
+  const NotificationList({required this.username, required this.notifications});
+
+  static List<Notification> _parseNotificationList(List<dynamic> list) {
+    return List<Notification>.from(list.map((e) => Notification.fromJson(e)));
+  }
+
+  factory NotificationList.fromJson(Map<String, dynamic> jsonInit) {
+    return switch (jsonInit) {
+      {
+      "username": String username,
+      "notifications": List<dynamic> notifications,
+      } => NotificationList(
+        // username: username,
+        username: username,
+        notifications: _parseNotificationList(notifications)
+      ),
+      _ => throw const FormatException("failed to load NotificationList"),
+    };
+  }
+
+  @override
+  String toString() {
+    return 'NotificationList{username: $username, notifications: $notifications}';
+  }
+}
+
+class Notification {
+  final String title, body;
+  final int hour, minute;
+
+  const Notification({
+    required this.title,
+    required this.body,
+    required this.hour,
+    required this.minute,
+  });
+
+  factory Notification.fromJson(Map<String, dynamic> jsonInit) {
+    return switch (jsonInit) {
+      {
+      "title": String title,
+      "body": String body,
+      "hour": int hour,
+      "minute": int minute,
+      } => Notification(
+        title: title,
+        body: body,
+        hour: hour,
+        minute: minute
+      ),
+      _ => throw const FormatException("failed to load Notification"),
+    };
+  }
+
+  Map<String, dynamic> toJson() => {
+    'title': title,
+    'body': body,
+    'hour': hour,
+    'minute': minute
+  };
+
+  @override
+  String toString() {
+    return 'Notification{title: $title, body: $body, hour: $hour, minute: $minute}';
+  }
+}

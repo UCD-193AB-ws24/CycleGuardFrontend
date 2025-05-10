8e6b49455992eb2b5c2646f8ffd67772b7abecb6
Braxai <ianoc2276@gmail.com>
2025-05-10 12:05:01 -0700

    Lighter onboard background color

--- lib/pages/create_account.dart
+++ lib/pages/create_account.dart
@@ -196,14 +196,14 @@ class LoginFormState extends State<CreateAccountPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFF5E7C4),
+      backgroundColor: Color(0xFFFAECCF),
       appBar: AppBar(
         title: Text(
             'Create Account',
             style: GoogleFonts.poppins(
                 textStyle: poppinsStyle
             )),
-        backgroundColor: Color(0xFFF5E7C4),
+        backgroundColor: Color(0xFFFAECCF),
       ),
       body: Center(
           child: ListView(
